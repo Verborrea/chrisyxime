cb288056b2e8e5f6b6fb9eba94149d2a43ee1d46
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2025-10-15 15:25:11 -0500

    first page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import header from '$lib/header.avif';
+
+	import cel from '$lib/hero/cel.avif';
+	import tab from '$lib/hero/tab.avif';
+	import comp from '$lib/hero/comp.avif';
+
+	import firma from '$lib/firma.avif';
+	import musicos from '$lib/musicos.avif';
+	import escudo from '$lib/escudo.avif';
+	import flor from '$lib/flor.avif';
+	import Calendar from './Calendar.svelte';
+</script>
+
+<header class="relative z-10">
+	<div class="h-20 bg-purple-light lg:h-28"></div>
+	<img
+		src={header}
+		alt="Escudo de los novios"
+		class="absolute top-3.5 left-1/2 max-w-24 -translate-x-1/2 rounded-[100%] bg-purple-light p-4 md:top-5 md:max-w-32 md:p-5"
+	/>
+</header>
+<article id="hero" class="overflow-hidden pt-32 pb-18 sm:pt-48 md:pt-24 lg:-mt-18 lg:pt-0">
+	<picture>
+		<source srcset={cel} media="(max-width: 767px)" />
+		<source srcset={tab} media="(min-width: 768px) and (max-width: 1023px)" />
+		<source srcset={comp} media="(min-width: 1024px)" />
+		<img
+			src={comp}
+			alt="Catedral de arequipa junto con una palméra."
+			class="mx-auto w-full max-w-350 -translate-x-4 scale-150 sm:scale-125 md:translate-x-0 md:scale-100"
+		/>
+	</picture>
+</article>
+<main class="px-6 sm:text-lg">
+	<div class="flex flex-col items-center gap-16 text-center sm:gap-20">
+		<div class="flex flex-col items-center gap-2 tracking-[0.2em] sm:tracking-[0.4em]">
+			<img src={firma} alt="Firma de los novios" class="mx-auto w-full max-w-70 pb-2" />
+			<p class="text-sm sm:text-lg">SÁBADO, 20 DE JUNIO, 2026</p>
+			<p class="text-sm sm:text-lg">AREQUIPA, PERÚ</p>
+		</div>
+		<img src={musicos} alt="Músicos" class="mx-auto w-full max-w-130" />
+		<div class="flex flex-col items-center">
+			<p>Nuestro</p>
+			<p class="pt-3 font-serif text-6xl">DÍA ESPECIAL</p>
+			<p>se acerca...</p>
+		</div>
+		<Calendar />
+		<div class="flex flex-col items-center gap-4">
+			<p class="max-w-120 text-balance">
+				Guarda la fecha y acompáñanos al viaje <strong class="font-bold"
+					>más importante de nuestras vidas</strong
+				>.
+			</p>
+			<div class="h-[1px] w-16 bg-black"></div>
+			<p>¡La invitación oficial llegará pronto!</p>
+		</div>
+		<a
+			href="/"
+			class="btn btn-purple max-w-100 flex-col gap-[3px] px-6 pt-5.5 pb-5 text-center sm:px-8 sm:py-6 sm:pt-6"
+		>
+			<p class="leading-[1.2] font-bold sm:text-xl">¿Nos visitas desde otro país?</p>
+			<p class="text-sm leading-[1.3] sm:text-base md:text-lg">
+				Ten algunas recomendaciones para tu viaje. <strong class="font-bold">Haz click aquí.</strong
+				>
+			</p>
+		</a>
+		<div class="flex flex-col gap-8">
+			<img src={escudo} alt="Escudo de los novios" class="w-32" />
+			<p>2 0 . 0 6 . 2 0 2 6</p>
+		</div>
+		<img src={flor} alt="Flor decorativa" class="mx-auto w-full max-w-250" />
+	</div>
+</main>
---
+++ src/routes/Calendar.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let months = $state(0);
+	let weeks = $state(0);
+	let days = $state(0);
+
+	const targetDate = new Date('2026-06-20T00:00:00');
+
+	function updateCountdown() {
+		const now = new Date();
+		const diff = targetDate.getTime() - now.getTime();
+
+		if (diff <= 0) {
+			months = weeks = days = 0;
+			return;
+		}
+
+		const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+		months = Math.floor(totalDays / 30.2); // promedio
+		const remainingDays = totalDays % 30.2;
+		weeks = Math.floor(remainingDays / 7);
+		days = Math.floor(remainingDays % 7);
+	}
+
+	onMount(() => {
+		updateCountdown();
+		const interval = setInterval(updateCountdown, 1000 * 60 * 60 * 12);
+		return () => clearInterval(interval);
+	});
+</script>
+
+<article class="grid grid-cols-5 items-center justify-center justify-items-center">
+	<section class="flex flex-col items-center pt-2">
+		<p class="font-serif text-6xl">{months}</p>
+		<p class="leading-none">meses</p>
+	</section>
+	<div class="h-24 w-[1px] bg-black"></div>
+	<section class="flex flex-col items-center pt-2">
+		<p class="font-serif text-6xl">{weeks}</p>
+		<p class="leading-none">semanas</p>
+	</section>
+	<div class="h-24 w-[1px] bg-black"></div>
+	<section class="flex flex-col items-center pt-2">
+		<p class="font-serif text-6xl">{days}</p>
+		<p class="leading-none">días</p>
+	</section>
+</article>
